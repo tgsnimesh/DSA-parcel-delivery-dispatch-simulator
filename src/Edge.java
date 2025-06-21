f292a734c77d14bacb373a1f6b69f1721aa35c83
tgsnimesh <tgsnimesh@gmail.com>
2025-06-21 16:50:28 +0530

    feat: Add Node and Edge classes to represent city graph Introduced Node and Edge classes to represent the city layout. Each node represents a delivery zone, and edges connect zones with weights representing travel times.

---
+++ src/Edge.java
@@ -0,0 +1,12 @@
+public class Edge {
+    public int to;
+    public int weight;
+    public Edge next;
+
+    public Edge(int to, int weight) {
+
+        this.to = to;
+        this.weight = weight;
+        this.next = null;
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    public int id;
+    public Edge head; // Linked list of edges
+
+    public Node(int id) {
+        this.id = id;
+        this.head = null;
+    }
+}
+
+
